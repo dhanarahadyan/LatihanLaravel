96562805d7d6a955b3ecfbcd70ff6d06f93028ea
dhanarahadyan1 <dhanarahadyan@yahoo.com>
2021-02-25 21:44:40 +0700

    Training about blade v2

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,9 @@
 <html>
     <head>
-        <title>App Name - @yield('title')</title>
+        <title>Child - @yield('title')</title>
+        <link rel = "icon" href =  
+"https://cdn.shopify.com/s/files/1/1061/1924/files/Hugging_Face_Emoji_2028ce8b-c213-4d45-94aa-21e1a0842b4d_large.png?15202324258887420558" 
+        type = "image/x-icon"> 
     </head>
     <body>
         @section('sidebar')
